a02aabce5765bea7afc9cff5c39c50127cf8b1e5
amin <garogigi@gmail.com>
2022-01-11 17:29:28 +0300

    add tests

---
+++ Nabidh2Test.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace nabidh;
+
+use PHPUnit\Framework\TestCase;
+
+class Nabidh2Test extends TestCase
+{
+
+    public function testCreateCanclePatientVisitMessage()
+    {
+
+    }
+
+    public function testAddPatientIdSegment()
+    {
+
+    }
+
+    public function testAddGroup()
+    {
+
+    }
+
+    public function testCreatePatientTransferMessage()
+    {
+
+    }
+
+    public function testCreateRegisterPatientMessage()
+    {
+
+    }
+
+    public function testCreateChangeOutpatientToInpatientMessage()
+    {
+
+    }
+
+    public function testInsertRegisterPatientMessageSegments()
+    {
+
+    }
+
+    public function testCreateMessage()
+    {
+
+    }
+
+    public function testCreateObservationMessage()
+    {
+
+    }
+
+    public function testCreateMergePatientIdMessage()
+    {
+
+    }
+
+    public function testAddPatientId()
+    {
+
+    }
+
+    public function testAddEvent()
+    {
+
+    }
+
+    public function testAddSegment()
+    {
+
+    }
+
+    public function testCreatePatientVisitMessage()
+    {
+
+    }
+
+    public function testCreateChangeInpatientToOutpatientMessage()
+    {
+
+    }
+
+    public function testCreatePharmacyTreatmentOrderMessage()
+    {
+
+    }
+}
