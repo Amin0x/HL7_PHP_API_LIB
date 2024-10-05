b844270dc7e57dfd4346f35ba1c2d2caf276fef7
Amin <49003095+Amin0x@users.noreply.github.com>
2024-10-05 23:12:46 +0300

    Update AmbulatoryStatus.php

--- src/tables/AmbulatoryStatus.php
+++ src/tables/AmbulatoryStatus.php
@@ -1,22 +1,33 @@
 <?php
 namespace amin0x\nabidh;
 
+/**
+ * Class representing different ambulatory statuses.
+ */
 class AmbulatoryStatus
 {
-    public const No_functional_limitations = 'A0';
-    public const Ambulates_with_assistive_device = 'A1';
-    public const Wheelchair_stretcher_bound = 'A2';
-    public const Comatose_non_responsive = 'A3';
-    public const Disoriented = 'A4';
-    public const Vision_impaired = 'A5';
-    public const Hearing_impaired = 'A6';
-    public const Speech_impaired = 'A7';
-    public const Non_English_speaking = 'A8';
-    public const Functional_level_unknown = 'A9';
-    public const Oxygen_therapy = 'B1';
-    public const Special_equipment = 'B2';
-    public const Amputee = 'B3';
-    public const Mastectomy = 'B4';
-    public const Paraplegic = 'B5';
-    public const Pregnant = 'B6';
+    public const NO_FUNCTIONAL_LIMITATIONS = 'A0';
+    public const AMBULATES_WITH_ASSISTIVE_DEVICE = 'A1';
+    public const WHEELCHAIR_STRETCHER_BOUND = 'A2';
+    public const COMATOSE_NON_RESPONSIVE = 'A3';
+    public const DISORIENTED = 'A4';
+    public const VISION_IMPAIRED = 'A5';
+    public const HEARING_IMPAIRED = 'A6';
+    public const SPEECH_IMPAIRED = 'A7';
+    public const NON_ENGLISH_SPEAKING = 'A8';
+    public const FUNCTIONAL_LEVEL_UNKNOWN = 'A9';
+    public const OXYGEN_THERAPY = 'B1';
+    public const SPECIAL_EQUIPMENT = 'B2';
+    public const AMPUTEE = 'B3';
+    public const MASTECTOMY = 'B4';
+    public const PARAPLEGIC = 'B5';
+    public const PREGNANT = 'B6';
+
+    /**
+     * Prevent instantiation of this class.
+     */
+    private function __construct()
+    {
+        // This class should not be instantiated.
+    }
 }
