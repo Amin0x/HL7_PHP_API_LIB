e8e107cdd33ea61252dcd75fbe374ae00c4f8ce1
Amin <49003095+Amin0x@users.noreply.github.com>
2024-10-05 23:10:41 +0300

    Update AdmissionType.php

--- src/tables/AdmissionType.php
+++ src/tables/AdmissionType.php
@@ -1,13 +1,25 @@
 <?php
 namespace amin0x\nabidh;
 
-class AdmissionType {
-    public const Accident = 'A';
-    public const Elective = 'C';
-    public const Emergency = 'E';
-    public const LaborAndDelivery = 'L';
-    public const Newborn= 'N';
-    public const Routine = 'R';
-    public const Urgent = 'U';
-    public const Other = 'O';
+/**
+ * Class representing different types of admissions.
+ */
+class AdmissionType
+{
+    public const ACCIDENT = 'A';
+    public const ELECTIVE = 'C';
+    public const EMERGENCY = 'E';
+    public const LABOR_AND_DELIVERY = 'L';
+    public const NEWBORN = 'N';
+    public const ROUTINE = 'R';
+    public const URGENT = 'U';
+    public const OTHER = 'O';
+
+    /**
+     * Prevent instantiation of this class.
+     */
+    private function __construct()
+    {
+        // This class should not be instantiated.
+    }
 }
