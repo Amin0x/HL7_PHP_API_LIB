1b84f91166924d2b03645dfdf6b2b295bb72fde5
Amin <49003095+Amin0x@users.noreply.github.com>
2024-10-05 23:07:24 +0300

    Update BedStatus.php

--- src/tables/BedStatus.php
+++ src/tables/BedStatus.php
@@ -1,12 +1,23 @@
 <?php
 namespace amin0x\nabidh;
 
+/**
+ * Class representing the status of a bed.
+ */
 class BedStatus
 {
-    public const Closed = 'C';
-    public const Housekeeping = 'H';
-    public const Occupied = 'O';
-    public const Unoccupied = 'U';
-    public const Contaminated = 'K';
-    public const Isolated = 'I';
+    public const CLOSED = 'C';
+    public const HOUSEKEEPING = 'H';
+    public const OCCUPIED = 'O';
+    public const UNOCCUPIED = 'U';
+    public const CONTAMINATED = 'K';
+    public const ISOLATED = 'I';
+
+    /**
+     * Prevent instantiation of this class.
+     */
+    private function __construct()
+    {
+        // This class should not be instantiated.
+    }
 }
